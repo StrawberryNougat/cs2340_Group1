d1cf538046ca063e26798d30ebc963328c401a2e
Taewoon Choi <taewoonchoi@Taewoons-MacBook-Air.local>
2023-06-15 20:59:12 -0400

    added riddle constructor

--- Logic_Masters_Games/src/main/java/com/example/logic_masters_games/Riddle.java
+++ Logic_Masters_Games/src/main/java/com/example/logic_masters_games/Riddle.java
@@ -8,4 +8,12 @@ public class Riddle {
     public String wrong_answer1;
 
     public String wrong_answer2;
+
+    public Riddle(String content, String difficulty, String right_answer, String wrong_answer1, String wrong_answer2) {
+        this.content = content;
+        this.difficulty = difficulty;
+        this.right_answer = right_answer;
+        this.wrong_answer1 = wrong_answer1;
+        this.wrong_answer2 = wrong_answer2;
+    }
 }
