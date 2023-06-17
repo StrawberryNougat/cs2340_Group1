b43dfe8d5f3e6d63aac82615640b06a4d8f03b4c
aaronhjs <ahjshin302@gmail.com>
2023-06-17 15:47:23 -0400

    added two unit cases (check select() pops from med stack, checking easy stack becomes empty)

--- Logic_Masters_Games/src/test/java/test_cases.java
+++ Logic_Masters_Games/src/test/java/test_cases.java
@@ -89,4 +89,77 @@ public class test_cases {
         Riddle riddle_test = riddleLibrary.select(2);
         assertTrue(riddleLibraryClone.availableRiddlesEasy.contains(riddle_test));
     }
+
+    @Test
+    public void test_riddleLibrary_select_med() throws IOException { //Aaron
+        Riddle a = new Riddle(
+                "David’s parents have three sons: Snap, Crackle, and what’s the name of the third son?",
+                "Easy",
+                "David",
+                "Snap",
+                "Crackle");
+        Riddle b = new Riddle(
+                "I follow you all the time and copy your every move, but you can’t touch me or catch me. What am I?",
+                "Easy",
+                "Your shadow",
+                "Your face",
+                "Your legs");
+        Riddle c = new Riddle(
+                "What has many keys but can’t open a single lock?",
+                "Easy",
+                "Piano",
+                "Guitar",
+                "Violin");
+
+        Riddle[] easies = {a};
+        Riddle[] mediums = {b};
+        Riddle[] hards = {c};
+
+        RiddleLibrary riddleLibrary = new RiddleLibrary(3, 5, easies, mediums, hards);
+        RiddleLibrary riddleLibraryClone = new RiddleLibrary(3, 5, easies, mediums, hards);
+
+        Riddle riddle_test = riddleLibrary.select(4);
+
+        // assert that riddle_test was indeed from the "med level" stack of riddles.
+        assertTrue(riddleLibraryClone.availableRiddlesMed.contains(riddle_test));
+    }
+
+    public void test_easyLibrary_becoming_empty() throws IOException { //Aaron
+        Riddle a = new Riddle(
+                "David’s parents have three sons: Snap, Crackle, and what’s the name of the third son?",
+                "Easy",
+                "David",
+                "Snap",
+                "Crackle");
+        Riddle b = new Riddle(
+                "I follow you all the time and copy your every move, but you can’t touch me or catch me. What am I?",
+                "Easy",
+                "Your shadow",
+                "Your face",
+                "Your legs");
+        Riddle c = new Riddle(
+                "What has many keys but can’t open a single lock?",
+                "Easy",
+                "Piano",
+                "Guitar",
+                "Violin");
+
+        // create arrays of riddles to pass into RiddleLibrary constructor.
+        Riddle[] easies = {a, a, a};
+        Riddle[] mediums = {b};
+        Riddle[] hards = {c};
+
+        // create a test RiddleLibrary object.
+        RiddleLibrary testRiddleLibrary = new RiddleLibrary(3, 5, easies, mediums, hards);
+
+        // call select with currScore that is less than EM_threshold
+        // for three times, so that the availableRiddlesEasy becomes empty.
+        Riddle riddle_test1 = testRiddleLibrary.select(2);
+        Riddle riddle_test2 = testRiddleLibrary.select(1);
+        Riddle riddle_test3 = testRiddleLibrary.select(0);
+
+        // assert that availableRiddlesEasy is empty.
+        assertTrue(testRiddleLibrary.availableRiddlesEasy.empty());
+    }
+
 }
