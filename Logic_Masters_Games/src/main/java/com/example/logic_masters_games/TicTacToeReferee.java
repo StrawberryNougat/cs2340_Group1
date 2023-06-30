c9d154e57431dda1a164355782f46b3e44e32718
aaronhjs <ahjshin302@gmail.com>
2023-06-30 11:11:55 -0400

    added Referee class

---
+++ Logic_Masters_Games/src/main/java/com/example/logic_masters_games/TicTacToeReferee.java
@@ -0,0 +1,51 @@
+package com.example.logic_masters_games;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TicTacToeReferee {
+
+    java.util.ArrayList<Integer> playerPositions = new java.util.ArrayList<>();
+    java.util.ArrayList<Integer> opponentPositions = new java.util.ArrayList<>();
+    java.util.ArrayList<List> winConditions = new ArrayList<>();
+
+    public void initializeWinConditions() {
+        // rows
+        List tRow = Arrays.asList(1, 2, 3);
+        List mRow = Arrays.asList(4, 5, 6);
+        List bRow = Arrays.asList(7, 8, 9);
+        // columns
+        List lCol = Arrays.asList(1, 4, 7);
+        List mCol = Arrays.asList(2, 5, 8);
+        List rCol = Arrays.asList(3, 6, 9);
+        // diagonals
+        List dia1 = Arrays.asList(1, 5, 9);
+        List dia2 = Arrays.asList(3, 5, 7);
+
+        winConditions.add(tRow);
+        winConditions.add(mRow);
+        winConditions.add(bRow);
+        winConditions.add(lCol);
+        winConditions.add(mCol);
+        winConditions.add(rCol);
+        winConditions.add(dia1);
+        winConditions.add(dia2);
+    }
+
+    public String checkWinner() {
+        // Check if it is a tie (e.g. board is full).
+        if (playerPositions.size() + opponentPositions.size() == 9) {
+            return "Tie!";
+        }
+        // Check if one side wins.
+        for (List cond : winConditions) {
+            if (playerPositions.containsAll(cond)) {
+                return "You won!";
+            } else if (opponentPositions.containsAll(cond)) {
+                return "Opponent won!";
+            }
+        }
+        return "";
+    }
+}
