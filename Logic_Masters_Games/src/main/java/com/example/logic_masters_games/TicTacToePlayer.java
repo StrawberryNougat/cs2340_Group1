f42bdc76a6d7ca50e47f67f6d185c2aba4ed4827
aaronhjs <ahjshin302@gmail.com>
2023-06-30 11:07:36 -0400

    added player and referee classes

---
+++ Logic_Masters_Games/src/main/java/com/example/logic_masters_games/TicTacToePlayer.java
@@ -0,0 +1,43 @@
+package com.example.logic_masters_games;
+
+public class TicTacToePlayer {
+    public void markBoard(String user, int pos, char[][] board) {
+        char symbol;
+        if (user.equals("p")) {
+            symbol = 'X';
+        } else {
+            symbol = 'O';
+        }
+        switch (pos) {
+            case 1:
+                board[0][0] = symbol;
+                break;
+            case 2:
+                board[0][2] = symbol;
+                break;
+            case 3:
+                board[0][4] = symbol;
+                break;
+            case 4:
+                board[2][0] = symbol;
+                break;
+            case 5:
+                board[2][2] = symbol;
+                break;
+            case 6:
+                board[2][4] = symbol;
+                break;
+            case 7:
+                board[4][0] = symbol;
+                break;
+            case 8:
+                board[4][2] = symbol;
+                break;
+            case 9:
+                board[4][4] = symbol;
+                break;
+            default:
+                break;
+        }
+    }
+}
