3f47b3701cdbf35ec8db1a31eb0e6318128b8b12
aaronhjs <ahjshin302@gmail.com>
2023-06-30 13:23:12 -0400

    Implemented restart game two and quit game buttons on lose screen

--- Logic_Masters_Games/src/main/java/com/example/logic_masters_games/GameTwoLoseController.java
+++ Logic_Masters_Games/src/main/java/com/example/logic_masters_games/GameTwoLoseController.java
@@ -1,12 +1,55 @@
 package com.example.logic_masters_games;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class GameTwoLoseController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     Label gameTwoLoseText = new Label("test label");
     public void setGameTwoLoseText(String text) {
         gameTwoLoseText.setText(text);
+    }
+
+    public void switchToStartScreen(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("game2-start-screen.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private Button quitButton;
+    @FXML
+    private AnchorPane scenePane;
+
+    public void quitGame(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("Are you sure you want to quit the game?");
+        alert.setContentText("This will close the application.");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("You successfully logged out!");
+            stage.close();
+        }
     }
 }
