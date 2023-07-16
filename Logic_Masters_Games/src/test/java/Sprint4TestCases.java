0c6ebdd5b393a90ae6141ffc886890f2a783bd43
Taewoon Choi <taewoonchoi@Taewoons-MacBook-Air.local>
2023-07-16 09:49:55 -0400

    I fixed my error for my two test cases, and fixed

--- Logic_Masters_Games/src/test/java/Sprint4TestCases.java
+++ Logic_Masters_Games/src/test/java/Sprint4TestCases.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 import static javafx.scene.paint.Color.AQUAMARINE;
 import static javafx.scene.paint.Color.YELLOW;
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Sprint4TestCases {
     @Test
     public void destroyTest() { //Aishi
@@ -46,53 +48,44 @@ public class Sprint4TestCases {
         //Failed destroy should return false
         assertFalse(block.destroy());
     }
-
-    @Test //Taewoon
-    public void testKaboom() throws NoSuchFieldException, IllegalAccessException {
-        // Create a BattleBoat with type 3 and vertical orientation
-        BattleBoat battleBoat = new BattleBoat(3, true);
-
-        // Access the private field 'strength' using reflection
-        Field strengthField = BattleBoat.class.getDeclaredField("strength");
-        strengthField.setAccessible(true);
-
-        // Get the initial value of 'strength'
-        int initialStrength = (int) strengthField.get(battleBoat);
-
-        // Call kaboom method once
-        battleBoat.kaboom();
-
-        // Get the updated value of 'strength'
-        int updatedStrength = (int) strengthField.get(battleBoat);
-
-        // Check if the strength is decremented by 1
-        assertEquals(initialStrength - 1, updatedStrength);
-    }
-
-    @Test //Taewoon
-    public void testHasSurvived() throws NoSuchFieldException, IllegalAccessException {
-        // Create a BattleBoat with type 2 and horizontal orientation
-        BattleBoat battleBoat = new BattleBoat(2, false);
-
-        // Access the private field 'strength' using reflection
-        Field strengthField = BattleBoat.class.getDeclaredField("strength");
-        strengthField.setAccessible(true);
-
-        // Set the value of 'strength' to 1
-        strengthField.set(battleBoat, 1);
-
-        // Check if the BattleBoat has initially survived
-        assertTrue(battleBoat.hasSurvived());
-
-        // Call kaboom method once
-        battleBoat.kaboom();
-
-        // Check if the BattleBoat has not survived anymore
-        assertFalse(battleBoat.hasSurvived());
-    }
-
-
+    @Test
+        public void testKaboom() {  //Taewoon
+            BattleBoat battleBoat = new BattleBoat(3, true);
+            int initialStrength = battleBoat.getStrength();
+            battleBoat.kaboom();
+            int updatedStrength = battleBoat.getStrength();
+            assertEquals(initialStrength - 1, updatedStrength);
+        }
         @Test
+        public void testHasSurvived() { //Taewoon
+            BattleBoat battleBoat = new BattleBoat(2, false);
+
+            // Assert that the BattleBoat initially has the correct strength
+            assertEquals(2, battleBoat.getStrength());
+
+            // Assert that the BattleBoat initially reports as survived
+            assertTrue(battleBoat.hasSurvived());
+
+            // Call kaboom() method to decrement the strength by 1
+            battleBoat.kaboom();
+
+            // Assert that the strength is now 1
+            assertEquals(1, battleBoat.getStrength());
+
+            // Assert that the BattleBoat still reports as survived
+            assertTrue(battleBoat.hasSurvived());
+
+            // Call kaboom() method again to decrement the strength by 1
+            battleBoat.kaboom();
+
+            // Assert that the strength is now 0
+            assertEquals(0, battleBoat.getStrength());
+
+            // Assert that the BattleBoat no longer reports as survived
+            assertFalse(battleBoat.hasSurvived());
+        }
+
+    @Test
         public void checkHitIsNullAfterClear () { // Eleanor
             Game3Controller game3Controller = new Game3Controller();
             game3Controller.setShipsToAdd(0);
